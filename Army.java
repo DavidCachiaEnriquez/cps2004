087db7cebd98aaccf70d6fd86f1a28585feb9119
David Cachia Enriquez <david.cachia-enriquez.21@um.edu.mt>
2023-01-17 16:31:27 +0100

    Creating Army class

---
+++ Army.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Army {
+
+    ArrayList<Troops> armyMembers = new ArrayList<Troops>();
+
+    void marching(){
+    }
+
+    void attacking(){
+    }
+
+    void defending(){
+    }
+
+    void powerCalculator(){
+    }
+
+    void killMembers(){
+    }
+
+}
