707ba12e1df2faff41988964485ec599889b869a
David Cachia Enriquez <david.cachia-enriquez.21@um.edu.mt>
2023-01-17 14:53:37 +0100

    Update Village + Troops

--- Troops.java
+++ Troops.java
@@ -1,3 +1,29 @@
 public class Troops {
-    
+    int cost;
+    int health;
+    int power;
+    int cc;
+    int speed;
+
+    void trainSoldier(){
+        health = 5;
+        power = 5;
+        cc = 3;
+        speed = 5;
+    }
+
+    void trainCavalry(){
+        health = 5;
+        power = 3;
+        cc = 5;
+        speed = 10;
+    }
+
+    void trainGiant(){
+        health = 10;
+        power = 7;
+        cc = 10;
+        speed = 3;
+    }
+
 }
--- Village.java
+++ Village.java
@@ -1,7 +1,7 @@
 public class Village {
     String playerName;
     int health;
-    int[][] location;
+    int location;
 
     Building buildings;
     Troops[] army;
@@ -9,10 +9,10 @@ public class Village {
 
     Village(String name){
         playerName = name;
+        health = 10;
 
         store = new Resources();
         buildings = new Building();
     }
-
 
 }
