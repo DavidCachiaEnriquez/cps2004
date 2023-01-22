99b1fc471a1aeb750c1c5ae3ac4d1551cab39be1
David Cachia Enriquez <david.cachia-enriquez.21@um.edu.mt>
2023-01-22 18:07:52 +0100

    set up cpp and hpp files + defined attributes and functions

---
+++ main.cpp
@@ -0,0 +1,35 @@
+# include <iostream>
+#include "main.hpp"
+
+using namespace std;
+
+gameManager::gameManager(){
+}
+
+bool gameManager::mineSetter(){
+    return false;
+}
+
+void gameManager::numSetter(){
+}
+
+void gameManager::displayGrid(){
+}
+
+void gameManager::updateGrid(){
+}
+
+void gameManager::winCheck(){
+}
+
+
+int validation::validator(){
+    return 0;
+}
+
+
+int main(){
+
+}
+
+
---
+++ main.hpp
@@ -0,0 +1,31 @@
+#include <string>
+using namespace std;
+
+class cell{
+    public:
+        string marker;
+        bool visibility;
+        bool mine;
+        int mineNum = 0;
+};
+
+class gameManager{
+    public:
+        int size = 16;
+        int numOfMines = 40;
+        int counter = 0;
+        cell grid[16][16];
+
+        gameManager();
+        bool mineSetter();
+        void numSetter();
+        void displayGrid();
+        void updateGrid();
+        void winCheck();
+};
+
+class validation{
+    public:
+        int validator();
+};
+
